ce5fab6a0db4dc52784828ad554b4d6cfdb23f52
Neal Gompa <neal@gompa.dev>
2026-04-05 07:48:19 -0400

    Fix license headers to match LGPLv2.1
    The license was downgraded to LGPL-2.1-or-later some time ago, but
    the license headers were not updated accordingly.
    
    Fixes: 0059982df5 ("change to LGPL 2.1")

--- src/btrfs.c
+++ src/btrfs.c
@@ -3,16 +3,16 @@
  * This file is part of btrfs-efi.
  *
  * btrfs-efi is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public Licence as published by
- * the Free Software Foundation, either version 3 of the Licence, or
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * btrfs-efi is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public Licence for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public Licence
+ * You should have received a copy of the GNU Lesser General Public License
  * along with btrfs-efi.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include <efibind.h>
